ec0275bd9dc76597adde70385dfb5a5a0bd460af
cdcalan <alinasami46@gmail.com>
2019-02-15 15:27:27 -0600

    scene update

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -5,12 +5,30 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let cnv;
+let buttonColor;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-}
+  noStroke();
+  buttonColor = 10;
+} 
 
 function draw() {
-  background(220);
+  background(200, 100, 100);
+  //Black Banner:
+  fill(0);
+  rect(0, 0, windowWidth, 100);
+  setButtonColor();
+  //Button:
+  fill (buttonColor);
+  rect(windowWidth/3, windowHeight/4, 100, 100); 
+  //Mouse Curser:
+  fill(220, 200, 200, 50);
   ellipse(mouseX, mouseY, 50, 50);
+
+}
+
+function setButtonColor() {
+  buttonColor = 255;
 }
