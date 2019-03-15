2e10b14c7e69175d81be8cc77f6cb6d2b8a9bf10
cdcalan <alinasami46@gmail.com>
2019-03-15 15:16:13 -0600

    states

--- 2-State Variables/sketch.js
+++ 2-State Variables/sketch.js
@@ -6,7 +6,11 @@
 // - describe what you did to take this project "above and beyond"
 
 
+////use time minis(), use screen sliding, use a rotating object. 
+
 let state;
+
+let buttonX;
 
 let fontSize = 30;
 let menuHeight = 600;
@@ -19,6 +23,8 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
   textSize(fontSize);
+
+  buttonX = width/2;
 }
 
 
@@ -46,7 +52,7 @@ function displayMenu() {
   //Menu Buttons:
   fill(255);
   for (let i=150; i<=menuHeight ; i+= 75) {
-    rect(width/2, i, buttonWidth, buttonHeight);
+    rect(buttonX, i, buttonWidth, buttonHeight);
     fill(100);
     text("Button 1", width/2 - buttonWidth/3, i + 10);
     fill(255);
@@ -55,6 +61,7 @@ function displayMenu() {
 
 
 function displayOther() {
+  background(255);
   fill(230, 100, 150);
   ellipse(450, 450, 200, 200);
 }
@@ -68,14 +75,11 @@ function mousePressed () {
   }
 }
 
+//an object is just a container of properties and etc.
 //define button x and y:
 function clickedOnButton(x, y) {
   return x >= buttonX - buttonWidth/2 &&
-         x <= buttonX + buttonWidth/2 &&
-         y >= buttonY - buttonHeight/2 &&
-         y <= buttonY + buttonHeight/2;
-
+         x <= buttonX + buttonWidth/2;
+      //   y >= buttonY - buttonHeight/2 &&
+        // y <= buttonY + buttonHeight/2;
 }
-
-
-//an object is just a container of properties and etc.
