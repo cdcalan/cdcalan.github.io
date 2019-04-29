ea6101d075303a8a1c54995a1f4bd8fd7da70ab6
cdcalan <alinasami46@gmail.com>
2019-04-29 15:19:52 -0600

    started major project sketch file

---
+++ Major Project/sketch.js
@@ -0,0 +1,21 @@
+// Major Project
+// Alina Sami
+// Monday, April 29, 2019
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+// To Do:
+// - create sprites classes
+// - use parent and subclasses of sprites.
+// - create background txt file (?)
+// - create a start screen with menu buttons: start, info
+// - Extra: loading animation? (maybe use css)?
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
---
+++ Major Project/sprites.js
@@ -0,0 +1,27 @@
+class sprites {
+  constructor() {
+    // Coordinates and speed variables:
+    this.x;
+    this.y;
+    this.dx;
+    this.dy;
+
+  }
+
+  show() {
+
+  }
+
+  //
+  move() {
+
+  }
+
+  // Checks if player has collided with sprite:
+  hasCollided() {
+
+  }
+
+
+
+}
