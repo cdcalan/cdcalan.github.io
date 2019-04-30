2b6b605346f6722adeb2acc7ded9210764f89162
cdcalan <alinasami46@gmail.com>
2019-04-30 15:20:30 -0600

    parent classes of sprites and buttons

--- Major Project/sketch.js
+++ Major Project/sketch.js
@@ -12,10 +12,60 @@
 // - create a start screen with menu buttons: start, info
 // - Extra: loading animation? (maybe use css)?
 
+let player;
+
+let screenState;
+
+class Button {
+  constructor() {
+    this.x = windowWidth/2;
+    this.y = windowHeight/2;
+    this.w = 150;
+    this.h = 50;
+  }
+  show(){
+
+  }
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  screenState = "Start Screen";
+
+  // Demo Sprite:
+  player = new User(300, windowHeight-250);
 }
+
+
+
 
 function draw() {
   background(220);
+
+  if (screenState === "Start Screen") {
+    displayStartScreen();
+  }
+  else if (screenState === "Game Screen") {
+    displayGameScreen();
+  }
+  else if (screenState === "Game Over") {
+    displayGameOverScreen();
+  }
+  // Optional: level up screens? or just messages?
+  
+  player.show();
+  player.move();
+  player.jump();
+}
+
+
+
+
+function displayStartScreen() {
+  background(50, 100, 150);
+
+  startButton.show();
+  infoButton.show();
+
 }
--- Major Project/sprites.js
+++ Major Project/sprites.js
@@ -1,27 +1,49 @@
-class sprites {
-  constructor() {
-    // Coordinates and speed variables:
-    this.x;
-    this.y;
-    this.dx;
-    this.dy;
+class Sprites {
+  constructor(x, y) {
+    // Coordinates:
+    this.x = x;
+    this.y = y;
+    // Dimensions:
+    this.h = 50;
+    this.w = 20;
+    // Speed variables:
+    this.dx = random(3, 10);
+    this.dy = 1;
+
 
   }
-
+  // Displays sprites:
   show() {
+    fill(100, 200, 150);
+    rect(this.x, this.y, this.w, this.h);
 
   }
 
-  //
+  // Sprite horizontal movement:
   move() {
-
+    if (keyIsPressed && keyCode === RIGHT_ARROW) {
+      this.x += this.dx;
+    }
   }
 
-  // Checks if player has collided with sprite:
+  // Checks if sprite has collided with player:
   hasCollided() {
 
   }
+}
 
 
 
+
+class User extends Sprites {
+  // Implement gravity!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  jump() {
+    if (keyIsPressed && keyCode === UP_ARROW) {
+      this.y -= this.dy;
+    }
+  }
+
+  attack() {
+
+  }
 }
